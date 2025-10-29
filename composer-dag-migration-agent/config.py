a7583d0eb514a03d85d6c671e9ba155d95841d54
AnikethDG <anikethd@google.com>
2025-10-29 05:12:23 +0000

    updated config file

--- composer-dag-migration-agent/config.py
+++ composer-dag-migration-agent/config.py
@@ -1,7 +1,7 @@
-
+import os
 class Config:
     def __init__(self):
-        self.project_id = 'ani123'
+        self.project_id = os.getenv('project_id')
         self.generative_model = "gemini-2.5-pro"
 
 config = Config()
